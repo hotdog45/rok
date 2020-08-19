36e1cccba1929cc4e8191de3f12894345326fd27
装点文化小开发 <qiancong@zhuangdianwenhua.com>
2020-08-19 09:21:31 +0800

    1

--- lib/common/net/rok_dao.dart
+++ lib/common/net/rok_dao.dart
@@ -1,5 +1,8 @@
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:rok/common/config/config.dart';
 import 'package:rok/common/net/address.dart';
 import 'package:rok/common/net/api.dart';
+import 'package:rok/common/unils/local_storage.dart';
 
 
 //获取版本更新数据
@@ -14,3 +17,15 @@ reqUserLogin(String phone, /*String smsCode,*/String password) async {
 
   });
 }
+
+//userProfit
+
+getAvailableBalance() async {
+  String token = await LocalStorage.get(Config.USER_TOKEN) ?? "";
+
+  Fluttertoast.showToast(msg: token+"ds");
+  return await httpManager.netFetch(userProfit, {
+    "token": token,
+  });
+}
+
