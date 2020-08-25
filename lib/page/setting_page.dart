97cb5b14c152670a6848971fec73e034e2d51fa2
装点文化小开发 <qiancong@zhuangdianwenhua.com>
2020-08-25 10:00:11 +0800

    1

---
+++ lib/page/setting_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:rok/common/config/config.dart';
+import 'package:rok/common/style/style.dart';
+import 'package:rok/common/unils/local_storage.dart';
+import 'package:rok/widget/common/yp_app_bar.dart';
+
+class SettingPage extends StatefulWidget {
+  @override
+  _SettingPageState createState() => _SettingPageState();
+}
+
+class _SettingPageState extends State<SettingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: YPAppBar(
+        "设置",
+      ).build(context),
+      body: Column(
+        children: <Widget>[
+          InkWell(
+            child: Container(
+                margin: EdgeInsets.only(left: 20, top: 30, right: 20),
+                height: 46,
+                width: double.infinity,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: kAppThemeColor,
+                  borderRadius: BorderRadius.circular(6),
+                ),
+                child: Text(
+                  "退出当前账户",
+                  style: TextStyle(
+                      color: Colors.white, fontSize: kAppFontSize(28)),
+                )),
+            onTap: () {
+              LocalStorage.remove(Config.USER_TOKEN);
+              LocalStorage.remove(Config.USER_MOBILE);
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
