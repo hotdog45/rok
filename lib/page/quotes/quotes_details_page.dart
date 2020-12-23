ce009288ad6d3d638b641234851eb8f62ee91af3
乔峰 <1@1.com>
2020-12-23 11:41:16 +0800

    横屏切换

--- lib/page/quotes/quotes_details_page.dart
+++ lib/page/quotes/quotes_details_page.dart
@@ -32,6 +32,9 @@ import 'package:rok/widget/transaction/transaction_widget.dart';
 import 'package:web_socket_channel/io.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
+import 'quotes_details_h_page.dart';
+import 'quotes_details_widget.dart';
+
 class QuotesDetailsPage extends StatefulWidget {
   @override
   _QuotesDetailsPageState createState() => _QuotesDetailsPageState();
@@ -39,68 +42,20 @@ class QuotesDetailsPage extends StatefulWidget {
 
 class _QuotesDetailsPageState extends State<QuotesDetailsPage>
     with SingleTickerProviderStateMixin {
-  List<KLineEntity> datas = [];
-  bool showLoading = true;
+
   bool showDrawer = false;
-  KLineDataController dataController = KLineDataController();
 
   var messageData ;
-
   StreamSubscription<DeviceOrientation> subscription;
-  WebSocketChannel channel = IOWebSocketChannel.connect(getWebSocketAddress(2));
 
   @override
   void initState() {
     super.initState();
 
-    dataController.changePeriodClick = (KLinePeriodModel model) {
-      channel.sink.close(messageData.goingAway);
-      getData(model.period);
-    };
-
     mController = TabController(
       length: tabTitles.length,
       vsync: this,
     );
-    channel.stream.listen((message) {
-      messageData = message;
-      var model = SocketBaseModel.fromJson(jsonDecode(message));
-      print("111111111111");
-      if (model != null && model.ch !=null && model.ch.startsWith("market.ethusdt.kline")) {
-        if (model.tick is Map){
-          print("333333333333333333");
-          var data = KLineEntity.fromJson(model.tick);
-          datas.add(data);
-        } else {
-          var list = jsonDecode(model.tick);
-          print("222222222");
-          datas = list
-              .map((item) => KLineEntity.fromJson(item))
-              .toList()
-              .reversed
-              .toList()
-              .cast<KLineEntity>();
-          channel.sink.close(message.goingAway);
-        }
-        DataUtil.calculate(datas);
-        showLoading = false;
-        setState(() {});
-      }
-    });
-    getData("1min");
-  }
-
-  void getData(String period) async {
-
-    //
-    channel.sink.add(
-        '{"event":"request","topic":"market.request","body":{ "type":1, "content":{ "ch":"market.ethusdt.kline.${period ?? "1min"}"}}} ');
-
-    if (period.startsWith("1min")){
-      channel.sink.add(
-          '{"event":"addTopic","topic":"market.ethusdt.kline.${period ?? "1min"}"} ');
-    }
-
   }
 
   @override
@@ -111,7 +66,6 @@ class _QuotesDetailsPageState extends State<QuotesDetailsPage>
     subscription?.cancel();
     super.dispose();
     mController.dispose();
-    channel.sink.close();
 
   }
 
@@ -157,9 +111,11 @@ class _QuotesDetailsPageState extends State<QuotesDetailsPage>
                     splashColor: Colors.transparent,
                     icon: Icon(Icons.fullscreen, size: 30),
                     onPressed: () {
+                      NavigatorUtils.navigatorRouter(context, QuotesDetailsHPage());
+
 //                      NavigatorUtils.showToast("全屏");
-                      OrientationPlugin.forceOrientation(
-                          DeviceOrientation.landscapeLeft);
+//                       OrientationPlugin.forceOrientation(
+//                           DeviceOrientation.landscapeLeft);
                     }),
                 IconButton(
                     highlightColor: Colors.transparent,
@@ -226,24 +182,7 @@ class _QuotesDetailsPageState extends State<QuotesDetailsPage>
                   ],
                 ),
               ),
-              Container(
-                width: double.infinity,
-                height: 450,
-                child: Stack(
-                  children: <Widget>[
-                    KLineVerticalWidget(
-                        datas: datas, dataController: dataController),
-                    Offstage(
-                      offstage: !showLoading,
-                      child: Container(
-                          width: double.infinity,
-                          height: 450,
-                          alignment: Alignment.center,
-                          child: CircularProgressIndicator()),
-                    ),
-                  ],
-                ),
-              ),
+              QuotesDetailsWidget(height: 450,),
               MyTabBar(mController: mController, tabTitles: tabTitles),
               Container(
                   width: double.infinity, height: 800, child: _tabBarView())
